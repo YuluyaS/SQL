9f86f44a0011c188a9c48f9defa73d3f46581217
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-12-28 11:03:47 +0400

    launch the jarnik

--- src/test/java/ru/netology/banklogin/data/SQLHelper.java
+++ src/test/java/ru/netology/banklogin/data/SQLHelper.java
@@ -1,4 +1,50 @@
 package ru.netology.banklogin.data;
 
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLHelper {
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    private SQLHelper() {
+    }
+
+    private static Connection getConn() throws SQLException{
+        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+    }
+
+    @SneakyThrows
+
+    public static getVerificationCode(){
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1 ";
+        try (var conn = getConn()) {
+
+        }
+    }
+
+    @SneakyThrows
+
+    public static void cleanDataBase() {
+        try (var conn = getConn()) {
+            QUERY_RUNNER.execute(conn, "DELETE FROM auth_codes");
+            QUERY_RUNNER.execute(conn, "DELETE FROM" card_transactions);
+            QUERY_RUNNER.execute(conn, "DELETE FROM" cards);
+            QUERY_RUNNER.execute(conn, "DELETE FROM" users);
+        }
+    }
+
+    @SneakyThrows
+
+    public static void cleanAuthCode(){
+        try (var conn = getConn()){
+            QUERY_RUNNER.execute(conn, "DELETE FROM auth_codes");
+        }
+    }
+
+
+
 }
--- src/test/java/ru/netology/banklogin/page/DashboardPage.java
+++ src/test/java/ru/netology/banklogin/page/DashboardPage.java
@@ -1,4 +1,17 @@
 package ru.netology.banklogin.page;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.banklogin.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class DashboardPage {
+    private final SelenideElement heading = $ ("[data-test-id=dashboard]");
+
+    public DashboardPage(){
+        heading.shouldHave(text("Личный кабинет")).shouldBe(visible);
+
+    }
 }
